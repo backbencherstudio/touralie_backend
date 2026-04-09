2476994b40523b12a0f170d80b1689e2020e3156
najim2004 <najim10112004@gmail.com>
2026-04-09 09:02:04 +0600

    feat: implement nest-commander bootstrap entry point for CLI execution

--- src/cmd.ts
+++ src/cmd.ts
@@ -1,13 +1,31 @@
 import 'dotenv/config';
 import { Module } from '@nestjs/common';
 import { CommandFactory } from 'nest-commander';
+import { ConfigModule } from '@nestjs/config';
+import { RedisModule } from '@nestjs-modules/ioredis';
+
 // internal imports
 import { PrismaService } from './prisma/prisma.service';
 import { SeedCommand } from './command/seed.command';
 import { RepositoryModule } from './common/repository/repository.module';
+import appConfig from './config/app.config';
 
 @Module({
-  imports: [RepositoryModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [appConfig],
+    }),
+    RedisModule.forRoot({
+      type: 'single',
+      options: {
+        host: appConfig().redis.host,
+        password: appConfig().redis.password,
+        port: +appConfig().redis.port,
+      },
+    }),
+    RepositoryModule,
+  ],
   providers: [SeedCommand],
 })
 export class AppModule {}
